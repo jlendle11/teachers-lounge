7b24fbb231a379e04626315507e24cb8008d9240
Justin Lendle <lendlejustin@gmail.com>
2019-08-26 14:27:39 -0400

    set up skeleton nav bar with Links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import './App.css';
+// Packages and Libraries
+import React from 'react'
+import { Route, Link } from 'react-router-dom'
+// Components
+import Homepage from './components/Homepage'
+import Profile from './components/Profile'
+// Styling 
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-
+      <nav className='nav-bar'>
+        <div>
+          <Link to="/" >Home</Link>
+        </div>
+        <div>
+          <Link to="/profile">Profile</Link>
+        </div>
+      </nav>
+      <main>
+        <Route 
+            exact path="/" 
+            component={Homepage}
+        />
+        <Route 
+            exact path="/profile" 
+            component={Profile}
+        />
+      </main>
     </div>
-  );
+  )
 }
 
 export default App;
