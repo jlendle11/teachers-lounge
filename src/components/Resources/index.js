caa83b853cf6a29ec93a1d4bf61b84a3c9969dc9
Alexander Barbaran <abarba02@gmail.com>
2019-08-29 11:10:33 -0400

    test

--- src/components/Resources/index.js
+++ src/components/Resources/index.js
@@ -4,6 +4,13 @@ function Resources() {
     return (
         <div>
             <h1>Resources</h1>
+            <h2>Recommendations</h2>
+            <div>
+                <h2>Metric Slides</h2>
+                <img/>
+                <h3></h3>
+            </div>
+
         </div>
     )
 }
