bc60b75661f620d7d06cc754d494ad07ac61afc9
Phil Ma <phillio411@gmail.com>
2019-08-29 11:01:15 -0400

    add hardcoded data

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -3,6 +3,67 @@ import Discussions from '../Discussions'
 import Resources from '../Resources'
 import Teachers from '../Teachers'
 
+const teachersArray = [
+    {
+        name: "Sally Jones",
+        subject: "Social Studies",
+        grade: 5,
+        location: "New York, NY"
+    },
+    {
+        name: "Jeff Smith",
+        subject: "Math",
+        grade: 8,
+        location: "White Plains, NY"
+    },
+    {
+        name: "Billie Jean",
+        subject: "Science",
+        grade: 4,
+        location: "Tarrytown, NY"
+    },
+    {
+        name: "Charlie Michaels",
+        subject: "Spanish",
+        grade: 11,
+        location: "Bronx, NY"
+    },
+    {
+        name: "Debbie Harold",
+        subject: "English",
+        grade: 7,
+        location: "Brooklyn, NY"
+    }
+]
+
+const resourcesArray = [
+    {
+        title: "Metric Slides",
+        type: "Slides",
+        subject: "Chemistry"
+    },
+    {
+        title: "Cool Animals",
+        type: "Activity",
+        subject: "Science"
+    },
+    {
+        title: "Title",
+        type: "Type",
+        subject: "Subject"
+    },
+    {
+        title: "Title",
+        type: "Type",
+        subject: "Subject"
+    },
+    {
+        title: "Title",
+        type: "Type",
+        subject: "Subject"
+    }
+]
+
 function Homepage() {
     return (
         <div>
