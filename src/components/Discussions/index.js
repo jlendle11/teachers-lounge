374930dffc120ae082d7a6cc4f0f4aa15bbe3526
Justin Lendle <lendlejustin@gmail.com>
2019-08-29 15:06:37 -0400

    add some things to discussions

--- src/components/Discussions/index.js
+++ src/components/Discussions/index.js
@@ -10,46 +10,46 @@ function Discussions() {
             </div>
             <div className="discuss-box box-one">
                 <div className="box-top">
-                    <p className="box-top-left box-bot-all">Russian Social Engineering<span className="discuss-span"> in Geopolotics</span></p>
-                    <p className="box-top-right box-bot-all">— posts</p>
+                    <p className="box-top-left box-all">Russian Social Engineering<span className="discuss-span"> in Geopolotics</span></p>
+                    <p className="box-top-right box-all">— posts</p>
                 </div>
                 <div className="box-bot">
-                    <p className="box-bot-left box-bot-all">By Fu45</p>
-                    <p className="box-bot-right box-bot-all">Last post 8:14pm</p>
+                    <p className="box-bot-left box-all">By Fu45</p>
+                    <p className="box-bot-right box-all">Last post 8:14pm</p>
                 </div>
             </div>
             <div className="discuss-box box-two">
             <div className="box-top">
-                    <p className="box-top-left box-bot-all">Wood Wid Web<span className="discuss-span"> in Mycology</span></p>
-                    <p className="box-top-right box-bot-all">420 posts</p>
+                    <p className="box-top-left box-all">Wood Wid Web<span className="discuss-span"> in Mycology</span></p>
+                    <p className="box-top-right box-all">420 posts</p>
                 </div>
                 <div className="box-bot">
-                    <p className="box-bot-left box-bot-all">By ShroomWiz</p>
-                    <p className="box-bot-right box-bot-all">Last post 8:14pm</p>
+                    <p className="box-bot-left box-all">By ShroomWiz</p>
+                    <p className="box-bot-right box-all">Last post 8:14pm</p>
                 </div>
             </div>
             <div className="discuss-box box-one">
                 <div className="box-top">
-                    <p className="box-top-left box-bot-all">Doh! How the Simpsons shaped the 90’s<span className="discuss-span"> in Funziez</span></p>
-                    <p className="box-top-right box-bot-all">1999 posts</p>
+                    <p className="box-top-left box-all">Doh! How the Simpsons shaped the 90’s<span className="discuss-span"> in Funziez</span></p>
+                    <p className="box-top-right box-all">1999 posts</p>
                 </div>
                 <div className="box-bot">
-                    <p className="box-bot-left box-bot-all">By IPfreely</p>
-                    <p className="box-bot-right box-bot-all">Last post 8:14pm</p>
+                    <p className="box-bot-left box-all">By IPfreely</p>
+                    <p className="box-bot-right box-all">Last post 8:14pm</p>
                 </div>
             </div>
             <div className="discuss-box box-two">
                 <div className="box-top">
-                    <p className="box-top-left box-bot-all">Who ate my stew?<span className="discuss-span"> in WTFrant</span></p>
-                    <p className="box-top-right box-bot-all">5 posts</p>
+                    <p className="box-top-left box-all">Who ate my stew?<span className="discuss-span"> in WTFrant</span></p>
+                    <p className="box-top-right box-all">5 posts</p>
                 </div>
                 <div className="box-bot">
-                    <p className="box-bot-left box-bot-all">By WhoatemyStu</p>
-                    <p className="box-bot-right box-bot-all">Last post 8:14pm</p>
+                    <p className="box-bot-left box-all">By WhoatemyStu</p>
+                    <p className="box-bot-right box-all">Last post 8:14pm</p>
                 </div>
             </div>
             <div className="discuss-foot-container">
-                <div className="discuss-foot">Go to all discussions</div>
+                <a className="discuss-foot" href="#" >Go to all discussions</a>
             </div>
         </div>
     )
