3d4b18a5a840cf3886ab31f2ceffd53243635137
Destin Coleman <destinc@Destins-MacBook-Air.local>
2019-08-29 22:00:25 -0400

    profile bio updated

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,52 +1,109 @@
 import React from 'react'
 import './Profile.css'
-import linda from '../../pics/linda.jpeg'
+// import Linda from '../../pics/Linda.jpeg'
 
 function Profile() {
     return (
-        <div className= "profile">
-            <div className="name-container">
-                <h1 className="name">Linda Cope</h1>
-                <div className="pic-info container">
-                    <div className="img-container">
-                        <img src={linda}  alt='pic'></img>
+        <div className="test-profile">
+
+            <div className="row-1">
+                <div className="box-1">
+
+                </div>
+                <div className="box-2">
+                <div
+                    className="bio-title"><h2 className="bio">Bio</h2>
+                </div>
+                <h3 className="bioText">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Suspendisse semper blandit dictum. Fusce sed quam sed est vehicula semper eu eget nunc. 
+                    Fusce vehicula venenatis est, non scelerisque lectus gravida scelerisque. Praesent molestie tincidunt nibh in gravida. 
+                    Fusce molestie, sapien a pellentesque rhoncus, orci lorem pharetra lectus, a hendrerit justo nisl sed odio.</h3>
+                <div className="passion-container">
+                   <div> 
+                       <h4 className="passions">Passions:</h4>
+                   </div>
+                   <div className="bio-buttons1">
+                       <button className="bio-button1" type="button">Geopolitics</button>
+                       <button className="bio-button2" type="button">Mycology</button>
+                       <button className="bio-button3" type="button">Fostering cats</button>
+                      </div>
+                      <div className="bio-buttons2">
+                       <button className="bio-button4" type="button">Kayaking</button>
+                       <button className="bio-button5" type="button">Crafts</button>
+                       <button className="bio-button6" type="button">Math</button>
                     </div>
-                    <div className="teacher-info">
-                        <div>Subject:Science</div>
-                        <div>Brooklyn High</div>
-                        <div>9 Years</div>
-                        <div>11-12</div>
-                        <div>Brooklyn,New York</div>
-                        <div>Interested in:</div>
-                        <div>   Collaboration</div>
-                        <div>   Friendship</div>
-                    </div>
+                </div>
                 </div>
             </div>
-            <button className="edit"type="button">Edit</button>
-            <div className="bio-container">
-                <div 
-                    className="bio-title"><h2>Bio</h2>
+
+
+            <div className="row-2">
+                <div className="box-3">
+
                 </div>
-                <h3 className="bioText">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sem et tortor consequat id porta nibh venenatis cras.</h3>
-                     <div passion-container>
-                     <h4>Passions:</h4> <button className="passion-button"type="button">Geopolitics</button>
-                     </div>
-            </div >
-                <div className="experience-container">
-                    <div 
-                        className="experience-title"><h2>Experience</h2>
-                    </div>
-                        <div className="experienceText">
-                            <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse semper blandit dictum. 
-                                Fusce sed quam sed est vehicula semper eu eget nunc. Fusce vehicula venenatis est, non scelerisque lectus gravida scelerisque. Praesent molestie tincidunt nibh in gravida. Fusce molestie, sapien a pellentesque rhoncus, orci lorem pharetra lectus, a hendrerit justo nisl sed odio. 
-                                Curabitur et risus felis. Sed euismod cursus laoreet. Quisque semper, tortor faucibus maximus imperdiet, neque nibh congue sapien, at elementum augue mauris nec nisi. In sodales sapien in nibh commodo sagittis. Aenean ac sodales risus. Nulla tincidunt et ipsum id imperdiet. Curabitur massa orci, viverra ac vestibulum id, pulvinar sit amet quam.
-                            </h3>   
-                        </div>    
+                <div className="box-4">
+
                 </div>
-                    
+            </div>
+
         </div>
+
+
+
+
+
+
+        // <div className="profile">
+        //     <div className="name-container">
+        //         <h1 className="name">Linda Cope</h1>
+        //         <div className="pic-info container">
+        //             <div className="img-container">
+        //                 <img src={linda} alt='pic'></img>
+        //             </div>
+        //             <div className="teacher-info">
+        //                 <div>Subject:Science</div>
+        //                 <div>Brooklyn High</div>
+        //                 <div>9 Years</div>
+        //                 <div>11-12</div>
+        //                 <div>Brooklyn,New York</div>
+        //                 <div>Interested in:</div>
+        //                 <div>   Collaboration</div>
+        //                 <div>   Friendship</div>
+        //             </div>
+        //         </div>
+        //     </div>
+        //     <button className="edit" type="button">Edit</button>
+
+
+
+        //     <div className="bio-container">
+        //         <div
+        //             className="bio-title"><h2>Bio</h2>
+        //         </div>
+        //         <h3 className="bioText">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        //              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sem et tortor consequat id porta nibh venenatis cras.</h3>
+        //         <div className="passion-container">
+        //             <h4>Passions:</h4> <button className="passion-button" type="button">Geopolitics</button>
+        //         </div>
+        //     </div>
+
+
+
+        //     <div className="experience-container">
+        //         <div
+        //             className="experience-title"><h2>Experience</h2>
+        //         </div>
+        //         <div className="experienceText">
+        //             <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse semper blandit dictum.
+        //                 Fusce sed quam sed est vehicula semper eu eget nunc. Fusce vehicula venenatis est, non scelerisque lectus gravida scelerisque. Praesent molestie tincidunt nibh in gravida. Fusce molestie, sapien a pellentesque rhoncus, orci lorem pharetra lectus, a hendrerit justo nisl sed odio.
+        //                 Curabitur et risus felis. Sed euismod cursus laoreet. Quisque semper, tortor faucibus maximus imperdiet, neque nibh congue sapien, at elementum augue mauris nec nisi. In sodales sapien in nibh commodo sagittis. Aenean ac sodales risus. Nulla tincidunt et ipsum id imperdiet. Curabitur massa orci, viverra ac vestibulum id, pulvinar sit amet quam.
+        //                     </h3>
+        //         </div>
+        //     </div>
+
+
+
+        // </div>
     )
 }
 
