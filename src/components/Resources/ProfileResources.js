35c31af4b0904525fe791100e87d3efa11ddb56d
Alexander Barbaran <abarba02@gmail.com>
2019-09-03 11:42:18 -0400

    profile resources

--- src/components/Resources/ProfileResources.js
+++ src/components/Resources/ProfileResources.js
@@ -58,52 +58,56 @@ const resourcesArray = [
 function ProfileResources() {
   return (
     <div className="resources-container-profile">
-      <div className="r-r-container">
-        <div className="r-r">Recomended Resources</div>
-        <div className="see-more">See more</div>
-      </div>
-      <div className="resources-component1">
-        {resourcesArray.map((resource, index) => {
-          return (
-            <div key={index} className="resources-content-container">
-              <div className="resources-boxes">
-                <div className="resources-title-box">
-                  <div className="resources-title">{resource.title}</div>
+      {/* ---------------------------- */}
+      <div className="r-r-c-c">
+        <div className="r-r-container">
+          <div className="r-r">Recomended Resources</div>
+          <div className="see-more">See more</div>
+        </div>
+        <div className="resources-component1">
+          {resourcesArray.map((resource, index) => {
+            return (
+              <div key={index} className="resources-content-container">
+                <div className="resources-boxes-profile">
+                  <div className="resources-title-box">
+                    <div className="resources-title">{resource.title}</div>
+                  </div>
+                  <div className="resources-img-box">
+                    <img className="resources-img" src={books} alt="books" />
+                  </div>
+                  <div className="resources-type">{resource.type}</div>
+                  <div className="resources-subject">{resource.subject}</div>
                 </div>
-                <div className="resources-img-box">
-                  <img className="resources-img" src={books} alt="books" />
-                </div>
-                <div className="resources-type">{resource.type}</div>
-                <div className="resources-subject">{resource.subject}</div>
               </div>
-            </div>
-          )
-        })}
+            )
+          })}
+        </div>
       </div>
       {/* ---------------------------- */}
-      <div className="r-v-r-container">
-        <div className="r-v-r">Recently Viewed Resources</div>
-        <div className="see-more">See more</div>
-      </div>
-      <div className="resources-component2">
-        {resourcesArray.map((resource, index) => {
-          return (
-            <div key={index} className="resources-content-container">
-              <div className="resources-boxes">
-                <div className="resources-title-box">
-                  <div className="resources-title">{resource.title}</div>
+      <div className="r-v-r-c">
+        <div className="r-v-r-container">
+          <div className="r-v-r">Recently Viewed Resources</div>
+          <div className="see-more">See more</div>
+        </div>
+        <div className="resources-component2">
+          {resourcesArray.map((resource, index) => {
+            return (
+              <div key={index} className="resources-content-container">
+                <div className="resources-boxes-profile">
+                  <div className="resources-title-box">
+                    <div className="resources-title">{resource.title}</div>
+                  </div>
+                  <div className="resources-img-box">
+                    <img className="resources-img" src={books} alt="books" />
+                  </div>
+                  <div className="resources-type">{resource.type}</div>
+                  <div className="resources-subject">{resource.subject}</div>
                 </div>
-                <div className="resources-img-box">
-                  <img className="resources-img" src={books} alt="books" />
-                </div>
-                <div className="resources-type">{resource.type}</div>
-                <div className="resources-subject">{resource.subject}</div>
               </div>
-            </div>
-          )
-        })}
+            )
+          })}
+        </div>
       </div>
-
     </div>
   )
 }
