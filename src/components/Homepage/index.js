0657498179fe74077535dad6e9c996c278dde6ff
Justin Lendle <lendlejustin@gmail.com>
2019-09-03 11:00:52 -0400

    remove homepage title

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -14,7 +14,6 @@ import './Homepage.css'
 function Homepage() {
     return (
         <div>
-            <h1>Homepage</h1>
             <Resources />
             <Teachers />
             <Discussions />
